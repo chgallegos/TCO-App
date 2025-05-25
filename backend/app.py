b2e27aeee5fbec36f78f7d76e47e4f7a7b496ce1
ChrisG <chrisale89@gmail.com>
2025-05-25 14:02:02 -0600

    modification to host and port

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
@@ -38,3 +39,6 @@ def compare():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
